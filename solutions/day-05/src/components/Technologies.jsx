31efb5be24a326866874af163965a396f4511577
thirdmadman <thirdmadman@gmail.com>
2023-05-09 16:41:31 +0200

    feat: lvl 3 task 1 solution

--- solutions/day-05/src/components/Technologies.jsx
+++ solutions/day-05/src/components/Technologies.jsx
@@ -5,38 +5,22 @@ import reactLogo from '../images/react.png';
 import './technologies.css';
 
 export function Technologies() {
-  const Html = () => (
-    <div className='logo-image'>
-      <img src={htmlLogo} alt='htmlLogo' />
-    </div>
-  );
 
-  const Css = () => (
-    <div className='logo-image'>
-      <img src={cssLogo} alt='cssLogo' />
-    </div>
-  );
-
-  const Js = () => (
-    <div className='logo-image'>
-      <img src={jsLogo} alt='jsLogo' />
-    </div>
-  );
-
-  const React = () => (
-    <div className='logo-image'>
-      <img src={reactLogo} alt='reactLogo' />
-    </div>
-  );
-
+  const LogoImage = (props) => {
+    const {image, alt} = props.imageData;
+    return (<div className='logo-image'>
+      <img src={image} alt={alt} />
+    </div>);
+  };
+  
   return (
     <div className='technologies'>
       <p className='technologies-title'>Front end technologies</p>
       <div className='technologies-logo-container'>
-        <Html />
-        <Css />
-        <Js />
-        <React />
+        <LogoImage imageData={{image: htmlLogo, alt: 'html'}} />
+        <LogoImage imageData={{image: cssLogo, alt: 'cssLogo'}} />
+        <LogoImage imageData={{image: jsLogo, alt: 'jsLogo'}} />
+        <LogoImage imageData={{image: reactLogo, alt: 'reactLogo'}} />
       </div>
     </div>
   );
